46d7a84050433c3917da1406b8df8ad98f04bfb4
alex <lucik904@yandex.ru>
2024-07-22 06:39:02 +0300

    add dependencies and binding init

--- app/src/main/java/com/example/newswave/presentation/MainActivity.kt
+++ app/src/main/java/com/example/newswave/presentation/MainActivity.kt
@@ -1,10 +1,7 @@
-package com.example.newswave
+package com.example.newswave.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.newswave.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
