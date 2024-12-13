f3ab78c41bd64519d6d50af27f99850ceaa5600c
alex <lucik904@yandex.ru>
2024-12-13 20:57:12 +0300

    Задокументирован NewsDetailsFragment и NewsDetailsViewModel

--- app/src/main/java/com/example/newswave/presentation/fragments/NewsDetailsFragment.kt
+++ app/src/main/java/com/example/newswave/presentation/fragments/NewsDetailsFragment.kt
@@ -1,6 +1,5 @@
 package com.example.newswave.presentation.fragments
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
@@ -15,7 +14,6 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.media3.common.MediaItem
@@ -25,11 +23,11 @@ import androidx.navigation.fragment.navArgs
 import com.example.newswave.R
 import com.example.newswave.app.NewsApp
 import com.example.newswave.databinding.FragmentNewsDetailsBinding
-import com.example.newswave.presentation.state.AuthState
 import com.example.newswave.presentation.MainActivity
+import com.example.newswave.presentation.adapters.CustomArrayAdapter
+import com.example.newswave.presentation.state.AuthState
 import com.example.newswave.presentation.viewModels.NewsDetailsViewModel
 import com.example.newswave.presentation.viewModels.ViewModelFactory
-import com.example.newswave.presentation.adapters.CustomArrayAdapter
 import com.example.newswave.utils.DateUtils
 import com.example.newswave.utils.LocaleHelper
 import com.example.newswave.utils.NetworkUtils
@@ -38,15 +36,14 @@ import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-
+/**
+ * Класс, представляющий экран с деталями новости
+ */
 class NewsDetailsFragment : Fragment() {
     private lateinit var binding: FragmentNewsDetailsBinding
     private val args by navArgs<NewsDetailsFragmentArgs>()
-
-    private lateinit var player: ExoPlayer
-
-
     private val viewModel: NewsDetailsViewModel by viewModels { viewModelFactory }
+    private lateinit var player: ExoPlayer
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
@@ -60,7 +57,6 @@ class NewsDetailsFragment : Fragment() {
         super.onAttach(context)
     }
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -71,32 +67,38 @@ class NewsDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        player = ExoPlayer.Builder(requireContext()).build()
-        binding.playerView.player = player
-        inflateFragment()
+        initializePlayer()
+        setupUI()
         observeViewModel()
-
-        val menuItemId = args.currentBottomItem
-        (activity as MainActivity).setSelectedMenuItem(menuItemId)
     }
 
-    private fun inflateFragment() {
+    // Инициализация видеоплеера
+    private fun initializePlayer() {
+        player = ExoPlayer.Builder(requireContext()).build()
+        binding.playerView.player = player
+    }
+
+    // Настройка пользовательского интерфейса
+    private fun setupUI() {
         setupTextViews()
         setupSpinner()
         setupImage()
         setupVideoPlayer()
         setupDefaultButton()
+        (activity as MainActivity).setSelectedMenuItem(args.currentBottomItem)
     }
 
+    // Настройка кнопки подписки
     private fun setupDefaultButton() {
         binding.btSubscription.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
-        if ("RU" == LocaleHelper.getSystemLanguage()) {
+        if (LANGUAGE_RU == LocaleHelper.getSystemLanguage()) {
             binding.btSubscription.setBackgroundResource(R.drawable.button_subscribe_rus)
         } else {
             binding.btSubscription.setBackgroundResource(R.drawable.button_subscribe)
         }
     }
 
+    // Настройка текстовых полей
     private fun setupTextViews() {
         with(binding) {
             tvText.text = args.news.text
@@ -105,40 +107,47 @@ class NewsDetailsFragment : Fragment() {
         }
     }
 
+    // Настройка выпадающего списка с авторами
     private fun setupSpinner() {
-        val list = args.news.author.split(",")
-        val spinner = binding.srAuthors
-
+        val authorsList = args.news.author.split(",")
         val customAdapter =
-            CustomArrayAdapter(requireContext(), R.layout.spinner_item, list)
-        customAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
-        spinner.adapter = customAdapter
-
-        spinner.adapter = customAdapter
-        if (!args.author.isNullOrBlank()) {
-            for (pos in 0 until customAdapter.count) {
-                if (customAdapter.getItem(pos).toString() == args.author) {
-                    spinner.setSelection(pos)
-                }
+            CustomArrayAdapter(requireContext(), R.layout.spinner_item, authorsList).apply {
+                setDropDownViewResource(R.layout.spinner_dropdown_item)
             }
+
+        with(binding.srAuthors) {
+            adapter = customAdapter
+            args.author?.let { preSelectAuthor(it, customAdapter) }
+            onItemSelectedListener = createSpinnerItemSelectedListener()
+            createSpinnerItemSelectedListener()
         }
+    }
 
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            @SuppressLint("ResourceAsColor")
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                if (p2 != 0) {
-                    Log.d("onItemSelectedListenerState", "fsfsd")
-                    val selectedItem = p0?.getItemAtPosition(p2).toString()
-                    viewModel.checkAuthorInRepository(selectedItem)
-                }
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                Log.d("TempLog", "TempLog")
+    // Предварительный выбор автора в списке
+    private fun preSelectAuthor(author: String, adapter: CustomArrayAdapter) {
+        for (pos in 0 until adapter.count) {
+            if (adapter.getItem(pos) == author) {
+                binding.srAuthors.setSelection(pos)
+                break
             }
         }
     }
 
+    // Создание слушателя выбора элемента в списке
+    private fun createSpinnerItemSelectedListener() = object : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            if (position != 0) {
+                val selectedAuthor = parent?.getItemAtPosition(position).toString()
+                viewModel.checkAuthorInRepository(selectedAuthor)
+            }
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+            Log.d(LOG_TAG, "No author selected in spinner");
+        }
+    }
+
+    // Настройка изображения
     private fun setupImage() {
         if (NetworkUtils.isNetworkAvailable(requireActivity().application)) {
             Picasso.get()
@@ -150,9 +159,10 @@ class NewsDetailsFragment : Fragment() {
         }
     }
 
+    // Настройка видеоплеера
     private fun setupVideoPlayer() {
         args.news.video?.let { videoUrl ->
-            if (!videoUrl.endsWith("m3u8")) {
+            if (!videoUrl.endsWith(M3U8_FORMAT)) {
                 binding.playerView.visibility = View.VISIBLE
                 val mediaItem = MediaItem.fromUri(Uri.parse(args.news.video))
                 player.setMediaItem(mediaItem)
@@ -162,73 +172,54 @@ class NewsDetailsFragment : Fragment() {
         }
     }
 
-    override fun onDestroy() { // crutch
-        super.onDestroy()
-        viewModel.clearState() // crutch
-    }
-
+    // Наблюдение за состоянием ViewModel
     private fun observeViewModel() {
         if (args.news.author == EMPTY_CATEGORY) {
             updateUIForUnknownAuthor()
             return
         }
 
-        viewLifecycleOwner.lifecycleScope.launch { // где надо и не надо collectlatest
-            repeatOnLifecycle(Lifecycle.State.CREATED) {
-                viewModel.user.collectLatest { isAuth ->
-                    Log.d("NewsDetailsFragmentState", isAuth.toString())
-                    handleAuthState(isAuth)
-                    if (isAuth is AuthState.LoggedOut) {
-                        if (findNavController().currentDestination?.id == R.id.newsDetailsFragment3) {
-                            findNavController().popBackStack(
-                                R.id.subscribedAuthorsFragment,
-                                false
-                            )
-                        }
-                        binding.btSubscription.visibility = View.VISIBLE
-                        return@collectLatest
-                    }
-                    viewModel.checkAuthorInRepository(args.news.author)
-                    viewModel.stateAuthor.collectLatest { isFavorite -> // возможно нужен запуск в launch
-                        Log.d("NewsDetailsFragmentState", isFavorite.toString())
-                        if (viewModel.isInternetConnection(requireContext())){
-                            if (isFavorite != null) {
-                                updateSubscriptionButton(isFavorite)
-                            }
-                        } else{
-                            updateSubscriptionButton(false)
-                            Log.d("CheckErrorMessage", "execute toast in newsdetails")
-                            showToastErrorInternetConnection()
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.authState.collectLatest { isAuth ->
+                    when (isAuth) {
+                        is AuthState.LoggedIn -> {
+                            handleLoggedInState()
+                            launch { observeAuthorState() }
                         }
 
+                        is AuthState.LoggedOut -> handleLoggedOutState()
                     }
                 }
             }
         }
     }
 
-
-    private fun handleAuthState(isAuth: AuthState) {
-            binding.btSubscription.setOnClickListener {
-                if (!viewModel.isInternetConnection(requireContext())){
-                    showToastErrorInternetConnection()
-                    return@setOnClickListener
-                }
-                    val checkSubscribed = binding.btSubscription.text.toString()
-                val author = binding.srAuthors.selectedItem.toString()
-                val btSub = requireActivity().getString(R.string.subscribe)
-                when (isAuth) {
-                    is AuthState.LoggedIn -> {
-                        if (checkSubscribed == btSub) viewModel.subscribeOnAuthor(author)
-                        else showUnsubscribeDialog(author)
-                    }
-
-                    is AuthState.LoggedOut -> requestLoginForSubscription()
-                }
+    // Действия при авторизованном пользователе
+    private fun handleLoggedInState() {
+        binding.btSubscription.setOnClickListener {
+            if (!viewModel.isInternetConnection(requireContext())) {
+                showToastErrorInternetConnection()
+                return@setOnClickListener
             }
-
+            val author = binding.srAuthors.selectedItem.toString()
+            val isSubscribed =
+                binding.btSubscription.text.toString() != getString(R.string.subscribe)
+            if (isSubscribed) showUnsubscribeDialog(author)
+            else viewModel.subscribeOnAuthor(author)
+        }
     }
 
+    // Действия при неавторизованном пользователе
+    private fun handleLoggedOutState() {
+        binding.btSubscription.visibility = View.VISIBLE
+        binding.btSubscription.setOnClickListener { requestLoginForSubscription() }
+        if (findNavController().currentDestination?.id == R.id.newsDetailsFragment3) {
+            findNavController().popBackStack(R.id.subscribedAuthorsFragment, false)
+        }
+    }
+
+    // Показ сообщения об отсутствии подключения к интернету
     private fun showToastErrorInternetConnection() {
         Toast.makeText(
             requireContext(),
@@ -237,10 +228,10 @@ class NewsDetailsFragment : Fragment() {
         ).show()
     }
 
-
+    // Запрос входа для подписки
     private fun requestLoginForSubscription() {
-        val builder = AlertDialog.Builder(requireContext())
-        builder.setTitle(getString(R.string.subscription_prompt_message))
+        AlertDialog.Builder(requireContext())
+            .setTitle(getString(R.string.subscription_prompt_message))
             .setMessage(getString(R.string.sign_in_required_message))
             .setPositiveButton(getString(R.string.sign_in_text)) { dialog, _ ->
                 launchSignInFragment()
@@ -249,73 +240,99 @@ class NewsDetailsFragment : Fragment() {
             .setNegativeButton(getString(R.string.cancel)) { dialog, _ ->
                 dialog.dismiss()
             }
-        builder.create().show()
+            .create()
+            .show()
     }
 
+    // Наблюдение за состоянием автора
+    private suspend fun observeAuthorState() {
+        viewModel.checkAuthorInRepository(args.news.author)
+        viewModel.authorState.collectLatest { isFavorite -> // возможно нужен запуск в launch
+            if (viewModel.isInternetConnection(requireContext())) {
+                if (isFavorite != null) {
+                    updateSubscriptionButton(isFavorite)
+                }
+            } else {
+                updateSubscriptionButton(false)
+                showToastErrorInternetConnection()
+            }
+        }
+    }
+
+    // Показ диалога для отмены подписки
     private fun showUnsubscribeDialog(author: String) {
-        val builder = AlertDialog.Builder(requireContext())
-        builder.setTitle(getString(R.string.confirmation))
-        builder.setMessage(getString(R.string.alert_dialog_question, author))
-
-        builder.setPositiveButton(getString(R.string.unsubscribe)) { dialog, _ ->
-            viewModel.unsubscribeFromAuthor(author)
-            dialog.dismiss()
-        }
-        builder.setNegativeButton(getString(R.string.cancel)) { dialog, _ ->
-            dialog.dismiss()
-        }
-
-        val dialog = builder.create()
-        dialog.show()
+        AlertDialog.Builder(requireContext())
+            .setTitle(getString(R.string.confirmation))
+            .setMessage(getString(R.string.alert_dialog_question, author))
+            .setPositiveButton(getString(R.string.unsubscribe)) { dialog, _ ->
+                viewModel.unsubscribeFromAuthor(author)
+                dialog.dismiss()
+            }
+            .setNegativeButton(getString(R.string.cancel)) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+            .show()
     }
 
+    // Обновление состояния кнопки подписки
     private fun updateSubscriptionButton(isFavorite: Boolean) {
-        if (isFavorite) {
-            setSubscribedButton()
-            binding.btSubscription.visibility = View.VISIBLE
-        } else {
-            setUnsubscribedButton()
-            binding.btSubscription.visibility = View.VISIBLE
-        }
+        if (isFavorite) setSubscribedButton() else setUnsubscribedButton()
+        binding.btSubscription.visibility = View.VISIBLE
     }
 
+    // Обновление интерфейса для неизвестного автора
     private fun updateUIForUnknownAuthor() {
-        binding.btSubscription.visibility = View.GONE
-        binding.srAuthors.visibility = View.GONE
-        binding.tvAuthorUnknown.visibility = View.VISIBLE
+        with(binding) {
+            btSubscription.visibility = View.GONE
+            srAuthors.visibility = View.GONE
+            tvAuthorUnknown.visibility = View.VISIBLE
+        }
     }
 
-
+    // Настройка кнопки для автора, на которого подписан пользователь
     private fun setSubscribedButton() {
-        binding.btSubscription.text = getString(R.string.subscribed)
-        binding.btSubscription.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
-        if ("ru" == LocaleHelper.getSystemLanguage()) {
-            Log.d("setSubscribedButton", "execute ed ru")
-            binding.btSubscription.setBackgroundResource(R.drawable.button_subscribed_rus)
-        } else {
-            Log.d("setSubscribedButton", "execute ed en")
-            binding.btSubscription.setBackgroundResource(R.drawable.button_subscribed)
+        with(binding.btSubscription) {
+            text = getString(R.string.subscribed)
+            setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+            setBackgroundResource(
+                if (LocaleHelper.getSystemLanguage() == LANGUAGE_RU) {
+                    R.drawable.button_subscribed_rus
+                } else {
+                    R.drawable.button_subscribed
+                }
+            )
         }
     }
 
+    // Настройка кнопки для автора, на которого не подписан пользователь
     private fun setUnsubscribedButton() {
-        binding.btSubscription.text = getString(R.string.subscribe)
-        binding.btSubscription.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
-        if ("ru" == LocaleHelper.getSystemLanguage()) {
-            Log.d("setSubscribedButton", "execute e ru")
-            binding.btSubscription.setBackgroundResource(R.drawable.button_subscribe_rus)
-        } else {
-            Log.d("setSubscribedButton", "execute e ru")
-            binding.btSubscription.setBackgroundResource(R.drawable.button_subscribe)
+        with(binding.btSubscription) {
+            text = getString(R.string.subscribe)
+            setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+            setBackgroundResource(
+                if (LocaleHelper.getSystemLanguage() == LANGUAGE_RU) {
+                    R.drawable.button_subscribe_rus
+                } else {
+                    R.drawable.button_subscribe
+                }
+            )
         }
     }
 
+    // Освобождение ресурсов при уничтожении представления
     override fun onDestroyView() {
         super.onDestroyView()
         player.release()
         binding.playerView.player = null
     }
 
+    override fun onDestroy() { // crutch
+        super.onDestroy()
+        viewModel.clearState()
+    }
+
+    // Запуск фрагмента авторизации
     private fun launchSignInFragment() {
         binding.btSubscription.visibility = View.GONE
         findNavController().navigate(
@@ -325,5 +342,8 @@ class NewsDetailsFragment : Fragment() {
 
     companion object {
         private const val EMPTY_CATEGORY = "unknownAuthor"
+        private const val LANGUAGE_RU = "ru";
+        private const val M3U8_FORMAT = "m3u8";
+        private const val LOG_TAG = "NewsDetailsLog"
     }
 }
--- app/src/main/java/com/example/newswave/presentation/viewModels/NewsDetailsViewModel.kt
+++ app/src/main/java/com/example/newswave/presentation/viewModels/NewsDetailsViewModel.kt
@@ -16,9 +16,15 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+/**
+ *  ViewModel для экрана новостей, который управляет состояниями подписки на автора
+ *  и авторизации пользователя
+ */
 class NewsDetailsViewModel @Inject constructor(
     private val favoriteAuthorCheckUseCase: FavoriteAuthorCheckUseCase,
     private val subscribeToAuthorUseCase: SubscribeToAuthorUseCase,
@@ -28,66 +34,72 @@ class NewsDetailsViewModel @Inject constructor(
     private val clearStateUseCase: ClearStateUseCase
 ) : ViewModel() {
 
-    private var _stateAuthor = MutableStateFlow<Boolean?>(null)
-    val stateAuthor: StateFlow<Boolean?> get() = _stateAuthor.asStateFlow()
+    // Состояние подписки на автора
+    private var _authorState = MutableStateFlow<Boolean?>(null)
+    val authorState: StateFlow<Boolean?> get() = _authorState.asStateFlow()
 
+    // Состояние пользователя
+    private var _authState = MutableStateFlow<AuthState>(AuthState.LoggedOut)
+    val authState: StateFlow<AuthState> get() = _authState.asStateFlow()
 
-    private var _user = MutableStateFlow<AuthState>(AuthState.LoggedOut)
-    val user: StateFlow<AuthState> get() = _user.asStateFlow()
+    init {
+        observeAuthState()
+    }
 
-
+    // Проверяет, добавлен ли автор в избранное
     fun checkAuthorInRepository(author: String) {
         viewModelScope.launch {
             favoriteAuthorCheckUseCase(author)
         }
     }
 
+    // Подписка пользователя на автора
     fun subscribeOnAuthor(author: String) {
         viewModelScope.launch {
             subscribeToAuthorUseCase(author)
         }
     }
 
+    // Отписка пользователя от автора
     fun unsubscribeFromAuthor(author: String) {
         viewModelScope.launch {
             unsubscribeFromAuthorUseCase(author)
         }
     }
 
+    // Наблюдает за изменениями в состоянии авторизации пользователя
     private fun observeAuthState() {
         viewModelScope.launch {
-            observeAuthStateUseCase().collect { userFirebase ->
-                if (userFirebase != null) {
-                    Log.d("CheckStateExecute", "observeAuthStateIf")
-                    _user.value = AuthState.LoggedIn(userFirebase)
-                    observeFavoriteAuthor()
-                } else {
-                    Log.d("CheckStateExecute", "observeAuthStateElse")
-                    _user.value = AuthState.LoggedOut
+            observeAuthStateUseCase()
+                .collect { userFirebase ->
+                    if (userFirebase != null) {
+                        _authState.value = AuthState.LoggedIn(userFirebase)
+                        observeFavoriteAuthor()
+                    } else {
+                        _authState.value = AuthState.LoggedOut
+                    }
                 }
-            }
         }
     }
 
+    // Наблюдает за состоянием избранного автора
     private fun observeFavoriteAuthor() {
         viewModelScope.launch {
-            isFavoriteAuthorUseCase().collectLatest { state ->
-                Log.d("CheckStateExecute", "observeFavoriteAuthor $state")
-                if (state == null) return@collectLatest
-                _stateAuthor.value = state
-            }
+            isFavoriteAuthorUseCase()
+                .filterNotNull()
+                .collectLatest { state ->
+                    _authorState.value = state
+                }
         }
     }
 
+    // Проверяет наличие подключения к интернету
     fun isInternetConnection(context: Context): Boolean {
         return NetworkUtils.isNetworkAvailable(context)
     }
 
+    // Очищает состояния ViewModel
     fun clearState() {
         clearStateUseCase()
-    }
-
-    init {
-        observeAuthState()
     }
 }
